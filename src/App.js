3774699a68590b1072788037d631b4478686d6fe
shravanchk <shravanchk@gmail.com>
2020-04-14 17:59:12 +0530

    updating our app with shop page, collection preview and collection item component.

--- src/App.js
+++ src/App.js
@@ -2,17 +2,23 @@ import React from 'react';
 import './App.css';
 import {Route} from 'react-router-dom'
 import HomePage from './pages/homepage/homepage.component'
+import ShopPage from './pages/shoppage/shoppage.component'
 
-const HatsPage = () => (
-  <h1>Hats page</h1>
-)
+// const HatsPage = (props) => {
+//   console.log({props})
+//   return(
+//     <div>
+//     <h1>Hats page</h1>
+//     </div>
+//   )
+// }
 
 function App() {
   return (
     <div>
     
       <Route exact path='/' component={HomePage}/>
-      <Route  path='/hats' component={HatsPage}/>
+      <Route  path='/shop' component={ShopPage}/>
      
     </div>
     
---
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import './collection-preview.styles.scss';
+import CollectionItem from '../collection-item/collection-item.component'
+
+const CollectionPreview = ({title, items}) => (
+ <div className='collection-preview'>
+     <div className='title'>{title.toUpperCase()}</div>
+     <div className='preview'>
+          {
+              items
+              .filter((item, idx) => idx < 4) 
+              .map(({id, ...otherItemProps}) => (
+                <CollectionItem key={id} {...otherItemProps}/> 
+              ))
+          }
+     </div>
+ </div>   
+)
+
+
+export default CollectionPreview
